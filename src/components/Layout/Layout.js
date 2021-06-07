1a764155e2542388984141bc776e76d0151bc8c3
Rodo <rodrigodavico@gmail.com>
2021-06-07 20:12:37 -0300

    commit desafio estilos y home

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import NavBar from "../NavBar/NavBar"
 import Footer from "../Footer/Footer"
 import ItemListContainer from "../ItemListContainer/ItemListContainer"
 
-const Layout = (children) => {
+const Layout = () => {
     return(
         <div className="container-fluid d-flex flex-column p-0 min-vh-100">
             <NavBar />
